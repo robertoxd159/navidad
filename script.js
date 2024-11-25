5655793da55198404a695a6abd119706d86d99aa
robertoxd159 <62416973+robertoxd159@users.noreply.github.com>
2024-11-25 11:03:21 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+function mostrarInvitacion() {
+    // Mostrar la invitación
+    document.getElementById("invitacion").style.display = "block";
+    
+    // Reproducir la canción navideña
+    var cancion = document.getElementById("cancionNavidad");
+    cancion.play();
+}
